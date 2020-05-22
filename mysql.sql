21eaec236fec0a9f818b957e8e77d8f78b421916
Matthew Litvinov <litvinma773@hotmail.com>
2020-05-22 18:30:26 -0400

    xml to database

--- mysql.sql
+++ mysql.sql
@@ -3,10 +3,19 @@ CREATE DATABASE [IF NOT EXISTS] tma2_part2;
 
 -- create accounts table to facilitate login
 
-CREATE TABLE accounts (
+CREATE TABLE IF NOT EXISTS accounts (
     ID int NOT NULL AUTO_INCREMENT,
     UserName varchar(255) NOT NULL,
     Password varchar(255) NOT NULL,
     Email varchar(255) NOT NULL,
     PRIMARY KEY (ID)
+);
+
+--create tables to hold xml information
+
+CREATE TABLE IF NOT EXISTS Course (
+    CourseID INT NOT NULL,
+    CourseName VARCHAR(50) NOT NULL,
+    CourseDescription VARCHAR(255) NOT NULL,
+    PRIMARY KEY (CourseID),
 );
